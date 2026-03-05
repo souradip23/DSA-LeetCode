2ccaf8bc56a6daefabceb8aa0caeb7509bf75832
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-05 18:16:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,17 +1,29 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] row =new boolean[9][9];
-        boolean[][] col=new boolean[9][9];
-        boolean[][] box = new boolean[9][9];
+        // boolean[][] row =new boolean[9][9];
+        // boolean[][] col=new boolean[9][9];
+        // boolean[][] box = new boolean[9][9];
+        // for(int i=0;i<9;i++){
+        //     for(int j=0;j<9;j++){
+        //         if(board[i][j]=='.')continue;
+        //         int num=board[i][j]-'1';
+        //         int boxIndex=(i/3)*3+(j/3);
+        //         if(row[i][num] || col[j][num] || box[boxIndex][num])return false;
+        //         row[i][num]=true;
+        //         col[j][num]=true;
+        //         box[boxIndex][num]=true;
+        //     }
+        // }
+        // return true;
+
+        HashSet<String>set=new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.')continue;
-                int num=board[i][j]-'1';
-                int boxIndex=(i/3)*3+(j/3);
-                if(row[i][num] || col[j][num] || box[boxIndex][num])return false;
-                row[i][num]=true;
-                col[j][num]=true;
-                box[boxIndex][num]=true;
+                char num=board[i][j];
+                if(!set.add(num+"in row"+i)  || 
+                 !set.add(num+"in col"+j)    || 
+                 !set.add(num+"in box"+(i/3)+(j/3)))return false;
             }
         }
         return true;

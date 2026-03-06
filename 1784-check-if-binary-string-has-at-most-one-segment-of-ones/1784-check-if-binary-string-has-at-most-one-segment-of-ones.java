49b642bcbb87b33a4266f2b67a4072db2057cf0a
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-06 11:03:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-       HashSet<Character> set=new HashSet<>();
-       boolean  flag=false;
-       for( char ch:s.toCharArray()){
-        set.add(ch);
-        if(ch=='0'){
+    //    HashSet<Character> set=new HashSet<>();
+    //    boolean  flag=false;
+    //    for( char ch:s.toCharArray()){
+    //     set.add(ch);
+    //     if(ch=='0'){
+    //             flag=true;
+    //     }
+    //     if(ch=='1' && flag){
+    //         return false;
+    //     }
+    //    } 
+    //    return true;
+    
+        boolean flag=false;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
                 flag=true;
+            }
+            if(s.charAt(i)=='1' && flag){
+                return false;
+            }
         }
-        if(ch=='1' && flag){
-            return false;
-        }
-       } 
-       return true;
+        return true;
     }
 }

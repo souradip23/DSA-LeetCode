22ecb73037c3b4a34e33d54c560e670744f4d95a
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-08 17:29:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -1,29 +1,39 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
-        int m=image.length;
-        int n=image[0].length;
-        int [][] res=new int[m][n];
-        for(int i=0;i<m;i++){
-            int left=0;
-            int right=n-1;
-            while(left<right){
-                int temp=image[i][left];
-                image[i][left]=image[i][right];
-                image[i][right]=temp;
-                left++;
-                right--;
+
+          for(int[] num:image){
+            for(int i=0;i<(image[0].length+1)/2;i++){
+                int temp=num[i]^1;
+                num[i]=num[image[0].length-i-1]^1;
+                num[image[0].length-i-1]=temp;
             }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(image[i][j]==0){
-                    image[i][j]=1;
-                }else{
-                    image[i][j]=0;
-                }
-            }
-        }
-        return image;
+         }
+         return image;
+
+        // int m=image.length;
+        // int n=image[0].length;
+        // int [][] res=new int[m][n];
+        // for(int i=0;i<m;i++){
+        //     int left=0;
+        //     int right=n-1;
+        //     while(left<right){
+        //         int temp=image[i][left];
+        //         image[i][left]=image[i][right];
+        //         image[i][right]=temp;
+        //         left++;
+        //         right--;
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(image[i][j]==0){
+        //             image[i][j]=1;
+        //         }else{
+        //             image[i][j]=0;
+        //         }
+        //     }
+        // }
+        // return image;
 
     }
 }

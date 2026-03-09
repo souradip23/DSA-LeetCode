375fd179c1d3c1282df6ec8358a9f75bf394f4ca
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-09 11:49:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,28 +1,28 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int size=nums.length;
-        Arrays.sort(nums);
-        return nums[size/2];
+        // int size=nums.length;
+        // Arrays.sort(nums);
+        // return nums[size/2];
 
 
 
 
-        // int count = 0;
-        // int candidate = 0;
+        int count = 0;
+        int candidate = 0;
 
-        // for(int num : nums){
+        for(int num : nums){
 
-        //     if(count == 0){
-        //         candidate = num;
-        //     }
+            if(count == 0){
+                candidate = num;
+            }
 
-        //     if(num == candidate){
-        //         count++;
-        //     }else{
-        //         count--;
-        //     }
-        // }
+            if(num == candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
 
-        // return candidate;
+        return candidate;
     }
 }

0bc7b137eed7ae6fe17d8f97f94819577af3de0a
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-27 12:48:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,33 +1,35 @@
 class Solution {
-    private:
-    static  bool comp(pair<int,int>&a,pair<int,int>&b){
+private:
+    static bool comp(pair<int, int>& a, pair<int, int>& b) {
         return a.second < b.second;
     }
 
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        vector<pair<int,int>>v;
-        int size=profits.size();
-        for(int i=0;i<size;i++){
-            pair<int,int> p=make_pair(profits[i],capital[i]);
+    int findMaximizedCapital(int k, int w, vector<int>& profits,
+                             vector<int>& capital) {
+        vector<pair<int, int>> v;
+        int size = profits.size();
+        for (int i = 0; i < size; i++) {
+            pair<int, int> p = make_pair(profits[i], capital[i]);
             v.push_back(p);
         }
-        int profit=0;
-        sort(v.begin(),v.end(),comp);
-         priority_queue<int> pq;
+        int profit = 0;
+        sort(v.begin(), v.end(), comp);
+        priority_queue<int> pq;
 
         int i = 0;
 
         // take at most k projects
-        while(k--){
+        while (k--) {
             // add all affordable projects
-            while(i < size && v[i].second <= w){
+            while (i < size && v[i].second <= w) {
                 pq.push(v[i].first); // profit
                 i++;
             }
 
             // if no project available
-            if(pq.empty()) break;
+            if (pq.empty())
+                break;
 
             // take max profit
             w += pq.top();

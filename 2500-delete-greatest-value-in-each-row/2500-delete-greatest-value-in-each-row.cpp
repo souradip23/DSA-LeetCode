535ba5e9413bb814ebbe5a1db03b24b8a99123c0
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-28 12:59:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        for(auto& row:grid){
-            sort(row.begin(),row.end());
+        int m = grid.size();
+        int n = grid[0].size();
+        for (auto& row : grid) {
+            sort(row.begin(), row.end());
         }
-        int ans=0;
-        for(int col=n-1;col>=0;col--){
-            int maxi=0;
-            for(int row=0;row<m;row++){
-                maxi=max(maxi,grid[row][col]);
+        int ans = 0;
+        for (int col = n - 1; col >= 0; col--) {
+            int maxi = 0;
+            for (int row = 0; row < m; row++) {
+                maxi = max(maxi, grid[row][col]);
             }
-            ans+=maxi;
+            ans += maxi;
         }
-    return ans;
+        return ans;
     }
 };

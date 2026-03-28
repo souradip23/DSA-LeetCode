b90b74437d4a3cb5527f5a694f134e9dff13643b
Souradip <145707173+souradip23@users.noreply.github.com>
2026-03-28 11:57:51 +0530

    Time: 0 ms (100%), Space: 13.7 MB (11.03%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        priority_queue<int> pq;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+        }
+        int maxProduct = INT_MIN;
+        while (!pq.empty()) {
+            int first = pq.top();
+            pq.pop();
+            int second = pq.top();
+         
+            int product = ((first - 1) * (second - 1));
+            maxProduct = max(product, maxProduct);
+        }
+        return maxProduct;
+    }
+};

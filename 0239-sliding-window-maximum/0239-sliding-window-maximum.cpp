13fcecbd16e1df2fbe09c952dd9c406aa2eea112
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-01 18:38:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> pq;
-        vector<int> res;
-        for (int i = 0; i < k; i++) {
-            pq.push(make_pair(nums[i], i));
-        }
-        res.push_back(pq.top().first);
-        
-        for (int i = k; i < nums.size(); i++) {
-            while (!pq.empty() && pq.top().second <= i - k) {
-                pq.pop();
+
+        // Using Deque
+        deque<int> dq; // stores indices
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Remove indices out of window
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
             }
-            pq.push(make_pair(nums[i], i));
-            res.push_back(pq.top().first);
+
+            // Remove smaller elements from back
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            // Push current index
+            dq.push_back(i);
+
+            // Add to result when first window is complete
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
         }
-        return res;
+
+        return result;
+
+        // Using priority queue ----------------------------------->
+
+        // priority_queue<pair<int, int>> pq;
+        // vector<int> res;
+        // for (int i = 0; i < k; i++) {
+        //     pq.push(make_pair(nums[i], i));
+        // }
+        // res.push_back(pq.top().first);
+
+        // for (int i = k; i < nums.size(); i++) {
+        //     while (!pq.empty() && pq.top().second <= i - k) {
+        //         pq.pop();
+        //     }
+        //     pq.push(make_pair(nums[i], i));
+        //     res.push_back(pq.top().first);
+        // }
+        // return res;
     }
 };

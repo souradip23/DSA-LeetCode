a2aab085afca5b73c17584de9f5174c8a8e6cd89
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-01 18:46:34 +0530

    Time: 8 ms (88.58%), Space: 112.7 MB (90.35%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts,
+                      long long budget) {
+        deque<int> dq; // stores indices of chargeTimes (max at front)
+        long long sum = 0;
+        int left = 0, ans = 0;
+        for (int right = 0; right < chargeTimes.size(); right++) {
+            // Maintain decreasing deque (max at front)
+            while (!dq.empty() &&
+                   chargeTimes[dq.back()] <= chargeTimes[right]) {
+                dq.pop_back();
+            }
+            dq.push_back(right);
+            // Add running cost
+            sum += runningCosts[right];
+            // Shrink window if cost exceeds budget
+            while (!dq.empty() &&
+                   chargeTimes[dq.front()] + (right - left + 1) * sum >
+                       budget) {
+                // Remove from deque if needed
+                if (dq.front() == left) {
+                    dq.pop_front();
+                }
+                sum -= runningCosts[left];
+                left++;
+            }
+            // Update answer
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};

7cf929428843a1cc76d1f3d76bd7b7e6a3cf6c2a
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-02 18:16:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-                ListNode beforeHead = new ListNode(0);
+        ListNode beforeHead = new ListNode(0);
         ListNode afterHead = new ListNode(0);
 
         ListNode before = beforeHead;

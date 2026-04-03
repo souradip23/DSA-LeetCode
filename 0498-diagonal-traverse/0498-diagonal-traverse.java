b05d9a7544fdd2c7f36f2081d9d5707006473d88
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-03 13:28:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int n=mat.length; 
-        int m=mat[0].length;
-        int[] res=new int[n*m];
-        int idx=0;
-        for(int i=0;i<m+n-1;i++){
-            int row=(i<m)?0:i-m+1;
-            int col=(i<m)?i:m-1;
-                      ArrayList<Integer> temp = new ArrayList<>();
+        int n = mat.length;
+        int m = mat[0].length;
+        int[] res = new int[n * m];
+        int idx = 0;
+        for (int i = 0; i < m + n - 1; i++) {
+            int row = (i < m) ? 0 : i - m + 1;
+            int col = (i < m) ? i : m - 1;
+            ArrayList<Integer> temp = new ArrayList<>();
 
             while (row < n && col >= 0) {
                 temp.add(mat[row][col]);

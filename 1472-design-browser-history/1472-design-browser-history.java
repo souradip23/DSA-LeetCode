c2c54b9d9c035519c4c231315988ee864858cde8
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-03 12:03:59 +0530

    Time: 47 ms (98.46%), Space: 54.2 MB (6.82%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,37 @@
+class BrowserHistory {
+ private List<String> history;
+    private int curr;
+    public BrowserHistory(String homepage) {
+        
+         history = new ArrayList<>();
+        history.add(homepage);
+        curr = 0;
+    }
+    
+    public void visit(String url) {
+         while (history.size() > curr + 1) {
+            history.remove(history.size() - 1);
+        }
+        
+        history.add(url);
+        curr++;
+    }
+    
+    public String back(int steps) {
+         curr = Math.max(0, curr - steps);
+        return history.get(curr);
+    }
+    
+    public String forward(int steps) {
+             curr = Math.min(history.size() - 1, curr + steps);
+        return history.get(curr);
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */

88b6b4e84c2c2f952d9393ceeca8f387fa024252
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-03 11:33:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,15 +1,16 @@
 class ParkingSystem {
     private int[] spaces;
+
     public ParkingSystem(int big, int medium, int small) {
-        spaces=new int[3];
-        spaces[0]=big;
-        spaces[1]=medium;
-        spaces[2]=small;
+        spaces = new int[3];
+        spaces[0] = big;
+        spaces[1] = medium;
+        spaces[2] = small;
     }
-    
+
     public boolean addCar(int carType) {
-        if(spaces[carType-1] >0){
-            spaces[carType-1]--;
+        if (spaces[carType - 1] > 0) {
+            spaces[carType - 1]--;
             return true;
         }
         return false;

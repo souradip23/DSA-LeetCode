76dd6e7132d39cc859c13a13e350e811dfaeac91
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-04 15:02:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,9 +1,28 @@
 import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-        BigInteger a=new BigInteger(num1);
-        BigInteger b=new BigInteger(num2);
-        BigInteger c=a.add(b);
-        return c.toString();
+
+        //Using BigInteger 
+        // BigInteger a=new BigInteger(num1);
+        // BigInteger b=new BigInteger(num2);
+        // BigInteger c=a.add(b);
+        // return c.toString();
+
+        // using Manual code
+
+        StringBuilder res=new StringBuilder();
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        int carry=0;
+        while(i>=0 || j>=0 || carry!=0){
+            int digit1=(i>=0)?num1.charAt(i)-'0':0;
+            int digit2=(j>=0)?num2.charAt(j)-'0':0;
+            int sum=carry+digit1+digit2;
+            res.append(sum%10);
+            carry=sum/10;
+            i--;
+            j--;
+        }
+        return res.reverse().toString();
     }
 }

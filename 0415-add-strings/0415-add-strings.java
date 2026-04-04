5be3c42c5162f3ed1f71176654d2526e6ac4ca99
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-04 15:03:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,8 +1,10 @@
 import java.math.BigInteger;
+
 class Solution {
     public String addStrings(String num1, String num2) {
 
         //Using BigInteger 
+        
         // BigInteger a=new BigInteger(num1);
         // BigInteger b=new BigInteger(num2);
         // BigInteger c=a.add(b);
@@ -10,16 +12,16 @@ class Solution {
 
         // using Manual code
 
-        StringBuilder res=new StringBuilder();
-        int i=num1.length()-1;
-        int j=num2.length()-1;
-        int carry=0;
-        while(i>=0 || j>=0 || carry!=0){
-            int digit1=(i>=0)?num1.charAt(i)-'0':0;
-            int digit2=(j>=0)?num2.charAt(j)-'0':0;
-            int sum=carry+digit1+digit2;
-            res.append(sum%10);
-            carry=sum/10;
+        StringBuilder res = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+            int sum = carry + digit1 + digit2;
+            res.append(sum % 10);
+            carry = sum / 10;
             i--;
             j--;
         }

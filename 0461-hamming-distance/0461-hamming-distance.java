868d5ec79706ea95de46036b529731f837f82b99
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-06 12:50:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,5 +1,16 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-         return Integer.bitCount(x ^ y);
+        //In-built function -->
+
+        //  return Integer.bitCount(x ^ y);
+
+       // * Brian Kernighan’s Algorithm
+        int xor=x^y;
+        int count=0;
+        while(xor!=0){
+            xor=xor &(xor-1);
+            count++;
+        }
+        return count;
     }
 }

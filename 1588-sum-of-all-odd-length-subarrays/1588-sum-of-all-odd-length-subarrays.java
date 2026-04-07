6ebef9a29572e5acd7e9593475159efc7465e3ed
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-07 21:34:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,15 +1,15 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-              int n = arr.length;
+        int n = arr.length;
         int totalSum = 0;
-        
+
         for (int i = 0; i < n; i++) {
             int total = (i + 1) * (n - i);
             int oddCount = (total + 1) / 2;
-            
+
             totalSum += oddCount * arr[i];
         }
-        
-        return totalSum;  
+
+        return totalSum;
     }
 }

c0143bf6b0cae8cdc21ee0dd9ca83ede76b136fd
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-09 17:18:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -19,7 +19,8 @@ class Node {
 
 class Solution {
     public int maxDepth(Node root) {
-                if (root == null) return 0;
+        if (root == null)
+            return 0;
 
         int max = 0;
         for (Node child : root.children) {

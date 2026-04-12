9e6471324442ae1bc443391711cf1eb0bf5a4941
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-12 15:51:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,23 +10,26 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(
-                (a, b) -> Integer.compare(a.val,b.val));
-        for(ListNode list :lists){
+        PriorityQueue<ListNode> pq=new PriorityQueue<>(
+            (a,b)-> Integer.compare(a.val,b.val)
+        );
+        for(ListNode list : lists){
             if(list!=null){
-            pq.offer(list);
+                pq.offer(list);
             }
         }
         ListNode dummy=new ListNode(0);
         ListNode curr=dummy;
         while(!pq.isEmpty()){
-            ListNode node =pq.poll();
-            curr.next=node;
-            curr=curr.next;
-            if(node.next!=null){
+           ListNode node =pq.poll();
+           curr.next=node;
+           curr=curr.next;
+            if(node.next !=null){
                 pq.offer(node.next);
             }
         }
-        return dummy.next;
+
+
+    return dummy.next;
     }
 }

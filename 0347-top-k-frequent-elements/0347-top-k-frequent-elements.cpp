757a07c33edf4290440ea98323a09b42216f32c4
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-12 12:45:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int>res;
-        priority_queue<pair<int,int>>pq;
+        priority_queue<int>pq;
         unordered_map<int,int>mp;
         for(int num:nums){
             mp[num]++;
         }
-        for(auto m:mp){
-         pq.push({m.second,m.first});
+        for(auto &a: mp){
+            pq.push({a.second,a.first});
         }
-       while(k-- && !pq.empty()){
-        res.push_back(pq.top().second);
-        pq.pop();
-       }
+        while(k-- && !pq.empty()){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
     return res;
     }
 };

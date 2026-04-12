7e804be066e5a29a40cbd26e3bdef5a97756a372
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-12 15:31:50 +0530

    Time: 4 ms (41.1%), Space: 43.4 MB (30.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty() || (ch==')' && st.peek()!='(') || (ch=='}' && st.peek()!='{') || (ch==']' && st.peek()!='[')){
+                    return false;
+                }
+                st.pop();
+            }
+           
+        }
+    return st.isEmpty();
+    }
+}

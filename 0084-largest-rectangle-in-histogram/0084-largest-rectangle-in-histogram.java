06484c67ca49316a688ac22a460b9f48f5e95535
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-12 15:21:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,33 +1,33 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n=heights.length;
-        int[] nse=new int[n];
-        int[] pse=new int[n];
-        Stack<Integer> st=new Stack<>();
+      int n=heights.length;
+      int[] nse=new int[n];
+      int[] pse=new int[n];
+      Stack<Integer> st=new Stack<>();
+      for(int i=n-1;i>=0;i--){
+         while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
+            st.pop();
+         }
+         nse[i]=st.isEmpty() ? n : st.peek();
+         st.push(i);
+      }
 
-        // for nse   right -> left
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            nse[i]=st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-        st.clear();
-        // for pse  left -> right
-          for(int i=0;i<n;i++){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            pse[i]=st.isEmpty() ? -1: st.peek();
-            st.push(i);
-        }
-        int maxArea=0;
-        for(int i=0;i<n;i++){
-            int width=nse[i]-pse[i]-1;
-            int area=heights[i]*width;
-            maxArea=Math.max(maxArea,area);
-        }
-        return maxArea;
+  st.clear();
+
+            for(int i=0;i<n;i++){
+         while(!st.isEmpty() && heights[st.peek()]>= heights[i]){
+            st.pop();
+         }
+         pse[i]=st.isEmpty() ? -1 : st.peek();
+         st.push(i);
+      }
+    int maxArea=0;
+    for(int i=0;i<n;i++){
+        int width=nse[i]-pse[i]-1;
+        int area=heights[i]*width;
+        maxArea=Math.max(maxArea,area);
+    }
+
+return maxArea;
     }
 }

1d2bb899df2398a8ce5a577ed51b2526f0b8b7ff
Souradip <145707173+souradip23@users.noreply.github.com>
2026-04-23 12:49:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int n = intervals.length;
+        int[][] res = new int[n][2];
+        res[0] = intervals[0];
+        int e = 0;
+        for (int i = 0; i < n; i++) {
+            if (intervals[i][0] <= res[e][1]) {
+                res[e][1] = Math.max(res[e][1], intervals[i][1]);
+            } else {
+                e++;
+                res[e] = intervals[i];
+            }
+        }
+        return Arrays.copyOfRange(res, 0, e + 1);
+    }
+}

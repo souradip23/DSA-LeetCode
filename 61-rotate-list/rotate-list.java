591869b907db4e49ed10e8373ea5cf9a743870d1
Souradip <145707173+souradip23@users.noreply.github.com>
2026-05-05 18:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (25.10%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,7 +10,9 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+
            if(head==null||head.next==null||k==0)return head;
+           
         ListNode curr=head;
         int length=1;
         while(curr.next!=null){

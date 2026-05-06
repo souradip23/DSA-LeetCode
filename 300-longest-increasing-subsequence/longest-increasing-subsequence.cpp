ccb9b189fd00e60511a21188f25790c12aa36636
Souradip <145707173+souradip23@users.noreply.github.com>
2026-05-06 23:06:20 +0530

    Time: 7 ms (71.96%) | Memory: 14.1 MB (74.69%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+                vector<int>list;
+
+        for(int i=0;i<nums.size();i++){
+            auto it=lower_bound(list.begin(),list.end(),nums[i]);
+            if(it==list.end()){
+                list.push_back(nums[i]);
+            }else{
+                *it=nums[i];
+            }
+        }
+        
+        return list.size();
+    }
+};

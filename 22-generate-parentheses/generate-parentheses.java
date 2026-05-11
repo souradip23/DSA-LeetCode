59ba156b13a93b887d98bf797bfc1de8b318cc9f
Souradip <145707173+souradip23@users.noreply.github.com>
2026-05-11 17:44:25 +0530

    Time: 3 ms (15.23%) | Memory: 44.6 MB (66.89%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    static void generate(List<String>res,int n,int open,int closed,String str){
+        if(str.length()==2*n){
+            res.add(str);
+            return;
+        }
+        if(open<n){
+            generate(res,n,open+1,closed,str+"(");
+        }
+        if(closed<open){
+            generate(res,n,open,closed+1,str+")");
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> res=new ArrayList<>();
+        generate(res,n,0,0,"");
+    return res;
+    }
+}

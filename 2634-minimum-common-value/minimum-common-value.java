39e55049e0d0046e28f4f420139c46ebab4eade8
Souradip <145707173+souradip23@users.noreply.github.com>
2026-05-19 09:43:35 +0530

    Time: 16 ms (12.85%) | Memory: 93.4 MB (7.68%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,17 +1,18 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> set=new HashSet<>();
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int num :nums1){
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums1) {
             set.add(num);
         }
-        for(int num: nums2){
-            if(set.contains(num)){
+        for (int num : nums2) {
+            if (set.contains(num)) {
                 list.add(num);
             }
         }
         Collections.sort(list);
-        if(list.size()==0)return -1;
+        if (list.size() == 0)
+            return -1;
         return list.get(0);
     }
 }

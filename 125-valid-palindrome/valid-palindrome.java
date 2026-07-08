a280e5c8d8a0900b95a554da172d1cac55cbe694
Souradip <145707173+souradip23@users.noreply.github.com>
2026-07-09 00:20:57 +0530

    Time: 2 ms (99.26%) | Memory: 44 MB (96.39%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // String[] str=s.split("[^a-zA-Z0-9]");
+        // StringBuilder sb=new StringBuilder();
+        // StringBuilder reversesb=new StringBuilder();
+        // for(String st : str){
+        //     sb.append(st.toLowerCase());
+        //     reversesb.append(st.toLowerCase());
+        // }
+        
+        // reversesb.reverse();
+
+        // return sb.toString().equals(reversesb.toString());
+
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            char left=s.charAt(i);
+            char right=s.charAt(j);
+            if(!Character.isLetterOrDigit(left)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(right)){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(left) != Character.toLowerCase(right)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}

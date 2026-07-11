123428c456a1b934eb3c7f278c0e98c95834a71d
Souradip <145707173+souradip23@users.noreply.github.com>
2026-07-11 10:57:36 +0530

    Time: 27 ms (98.93%) | Memory: 59 MB (74.02%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,39 +1,36 @@
 class Solution {
-    public void twoSumHelper(int f,int[] nums,List<List<Integer>> ans){
+    public void twoSumHelper(int f,int []nums,List<List<Integer>> list){
         int i=f+1;
         int j=nums.length-1;
         while(i<j){
             int sum=nums[f]+nums[i]+nums[j];
-            if(sum < 0){
+            if(sum<0){
                 i++;
             }else if(sum >0){
                 j--;
             }else{
-                ans.add(Arrays.asList(nums[f],nums[i],nums[j]));
+                list.add(Arrays.asList(nums[f],nums[i],nums[j]));
                 i++;
                 j--;
-
                 while(i<j && nums[i]==nums[i-1]){
                     i++;
                 }
                 while(i<j && nums[j]==nums[j+1]){
                     j--;
                 }
-
             }
         }
+
     }
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans=new ArrayList<>();
+        List<List<Integer>> list=new ArrayList<>();
         for(int f=0;f<nums.length;f++){
-            if(nums[f]>0){
-                break;
-            }
+            if(nums[f]>0)break;
             if(f==0 || nums[f]!=nums[f-1]){
-                twoSumHelper(f,nums,ans);
+                twoSumHelper(f,nums,list);
             }
         }
-        return ans;
+        return list;
     }
 }

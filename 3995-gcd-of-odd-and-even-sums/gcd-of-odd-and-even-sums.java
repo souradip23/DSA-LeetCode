0f4732d73743ea934820695da7a044da1309afae
Souradip <145707173+souradip23@users.noreply.github.com>
2026-07-15 13:39:02 +0530

    Time: 1 ms (83.11%) | Memory: 42.3 MB (86.19%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int gcd(int i,int j){
+        if(j==0)return i;
+        return gcd(j,i%j);
+    }
+    public int gcdOfOddEvenSums(int n) {
+        int sumeven=n*n;
+        int sumodd=n*(n+1);
+        return gcd(sumeven,sumodd);
+    }
+}

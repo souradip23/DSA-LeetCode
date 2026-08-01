282ab584977ae85fb9746a229570ce02f5d17aee
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-01 12:02:02 +0530

    Time: 64 ms (29.16%) | Memory: 42.9 MB (41.06%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int n=nums.length;
-        return helper(nums,0,n-1)>=0;
+        int n = nums.length;
+        return helper(nums, 0, n - 1) >= 0;
     }
-    private int helper(int [] nums,int i,int j){
-        if(i==j)return nums[i];
+
+    private int helper(int[] nums, int i, int j) {
+        if (i == j)
+            return nums[i];
         return Math.max(
-           nums[i]-helper(nums,i+1,j),
-            nums[j]-helper(nums,i,j-1)
-        );
+                nums[i] - helper(nums, i + 1, j),
+                nums[j] - helper(nums, i, j - 1));
     }
 }

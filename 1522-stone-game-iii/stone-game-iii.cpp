f84b44da5970ebc22a2fd84cd78b955a5a14a1e0
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-03 12:34:00 +0530

    Time: 15 ms (82.12%) | Memory: 130.2 MB (98.37%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static inline string s[] = {"Bob", "Tie", "Alice"};
+    string stoneGameIII(vector<int>& A) {
+        int n = A.size();
+        int dp[4] = {0, 0, 0, 0};
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i & 3] = A[i] - dp[(i + 1) & 3];
+
+            if (i + 2 <= n)
+                dp[i & 3] = max(dp[i & 3], A[i] + A[i + 1] - dp[(i + 2) & 3]);
+
+            if (i + 3 <= n)
+                dp[i & 3] = max(dp[i & 3],
+                                A[i] + A[i + 1] + A[i + 2] - dp[(i + 3) & 3]);
+        }
+
+        return s[(dp[0] > 0) - (dp[0] < 0) + 1];
+    }
+};

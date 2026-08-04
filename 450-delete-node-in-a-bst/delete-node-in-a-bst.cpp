88f8f8f3dc0d53181fff17a718818157c02a184f
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-04 12:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 34.2 MB (99.27%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,45 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* findMin(TreeNode* root) {
-        while (root->left != nullptr) {
-            root = root->left;
+
+    TreeNode* findMin(TreeNode* root){
+        while(root->left!=nullptr){
+            root=root->left;
         }
         return root;
     }
 
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == nullptr)
-            return nullptr;
+        if(root==nullptr)return nullptr;
 
-        if (key < root->val) {
-            root->left = deleteNode(root->left, key);
-        } else if (key > root->val) {
-            root->right = deleteNode(root->right, key);
-        } else {
+        if(key < root->val){
+            root->left=deleteNode(root->left,key);
+        }else if( key > root->val){
+            root->right = deleteNode(root->right,key);
+        }else{
 
-            if (root->left == nullptr) {
-                TreeNode* temp = root->right;
+            if(root->left ==nullptr){
+                TreeNode* temp=root->right;
                 delete root;
                 return temp;
             }
-            if (root->right == nullptr) {
-                TreeNode* temp = root->left;
+            if(root->right==nullptr){
+                TreeNode* temp=root->left;
                 delete root;
                 return temp;
             }
 
-            TreeNode* succes = findMin(root->right);
-            root->val = succes->val;
+            TreeNode* succes=findMin(root->right);
+            root->val=succes->val;
 
-            root->right = deleteNode(root->right, succes->val);
+            root->right =deleteNode(root->right , succes->val);
         }
-        return root;
+    return root;
     }
 };

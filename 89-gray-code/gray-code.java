dbc9fc2363778372fd5b2a53b67b8ab67a068b0e
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-05 19:05:15 +0530

    Time: 5 ms (54.91%) | Memory: 53.7 MB (59.67%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        List<Integer> list =new ArrayList<>();
-        for(int i=0;i< (1<<n) ;i++){
-            list.add(i^(i>>1));
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < (1 << n); i++) {
+            list.add(i ^ (i >> 1));
         }
         return list;
     }

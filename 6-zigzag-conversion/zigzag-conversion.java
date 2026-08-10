2102929ddd748b0868e4b371b623baf8d47523f8
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-11 01:36:42 +0530

    Time: 5 ms (52.56%) | Memory: 46.6 MB (62.21%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,25 +1,25 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if(numRows==1 || s.length() <= numRows){
+        if (numRows == 1 || s.length() <= numRows) {
             return s;
         }
-        StringBuilder[] rows=new StringBuilder[numRows];
-        for(int i=0;i<numRows;i++){
-            rows[i]=new StringBuilder();
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
         }
-        int currRow=0;
-        boolean goingDown=true;
-        for(char ch :  s.toCharArray()){
+        int currRow = 0;
+        boolean goingDown = true;
+        for (char ch : s.toCharArray()) {
             rows[currRow].append(ch);
-            if(currRow==0){
-                goingDown=true;
-            }else if(currRow==numRows-1){
-                goingDown=false;
+            if (currRow == 0) {
+                goingDown = true;
+            } else if (currRow == numRows - 1) {
+                goingDown = false;
             }
-        currRow+=goingDown ? 1 : -1;
+            currRow += goingDown ? 1 : -1;
         }
-        StringBuilder result=new StringBuilder();
-        for(StringBuilder row:rows){
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
             result.append(row);
         }
         return result.toString();

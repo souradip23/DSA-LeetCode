d84aa6b33f259a6a948c1a18a2a6b08726fe1a2c
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-23 19:43:09 +0530

    Time: 18 ms (13.12%) | Memory: 90.7 MB (11.12%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,24 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-    //   return 0;  
-    int maxi=Integer.MIN_VALUE;
-    for(int i=0;i<nums.length;i++){
-        maxi=Math.max(maxi,nums[i]);
-    }
-    int xor=0;
-    Set<Integer> set=new HashSet<>();
-    for(int num: nums){
-        set.add(num);
-    }
-    int index=0;
-    for(int i=1;i<=maxi;i++){
-        if(!set.contains(i)){
-            return i;
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            maxi = Math.max(maxi, nums[i]);
         }
-        index=i;
+        int xor = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int index = 0;
+        for (int i = 1; i <= maxi; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+            index = i;
 
-    }
-    
-    return index+1;
+        }
+        return index + 1;
     }
 }

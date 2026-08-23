d23b4c52903e08609858b4701a0c4531a2455980
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-23 19:40:11 +0530

    Time: 14 ms (26.17%) | Memory: 93.2 MB (7.64%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+    //   return 0;  
+    int maxi=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+        maxi=Math.max(maxi,nums[i]);
+    }
+    int xor=0;
+    Set<Integer> set=new HashSet<>();
+    for(int num: nums){
+        set.add(num);
+    }
+    int index=0;
+    for(int i=1;i<=maxi;i++){
+        if(!set.contains(i)){
+            return i;
+        }
+        index=i;
+
+    }
+    
+    return index+1;
+    }
+}

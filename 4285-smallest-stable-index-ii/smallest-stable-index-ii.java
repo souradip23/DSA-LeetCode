72f4ec096c26e4d86c898a9bede1f6b6a3e9898e
Souradip <145707173+souradip23@users.noreply.github.com>
2026-09-05 10:22:10 +0530

    Time: 3 ms (100.00%) | Memory: 125.3 MB (98.09%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int ansIdx = 0;         // index we're currently testing as the answer
-        int globalMax = Integer.MIN_VALUE;        // biggest number seen anywhere so far
+        int ansIdx = 0; // index we're currently testing as the answer
+        int globalMax = Integer.MIN_VALUE; // biggest number seen anywhere so far
         int ansMax = Integer.MIN_VALUE; // biggest number up to ansIdx
         for (int i = 0; i < n; i++) {
             globalMax = Math.max(globalMax, nums[i]);

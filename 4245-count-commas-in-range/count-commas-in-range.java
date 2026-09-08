4aac16a7335f1fcff995a85f88e614a75475941d
Souradip <145707173+souradip23@users.noreply.github.com>
2026-09-08 09:18:45 +0530

    Time: 8 ms (8.47%) | Memory: 42.9 MB (8.32%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        String s=String.valueOf(n);
-        // System.out.println(s);
-        if(s.length() <= 3)return 0; //edge case 
+        // String s=String.valueOf(n);
+        // // System.out.println(s);
+        // if(s.length() <= 3)return 0; //edge case 
         int count=0;
         for(int i=1;i<=n;i++){
             if(i>=1000){

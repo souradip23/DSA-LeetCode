37872e123068cde395f63810e69b9f48841b230e
Souradip <145707173+souradip23@users.noreply.github.com>
2026-09-14 10:49:04 +0530

    Time: 2 ms (70.21%) | Memory: 63.5 MB (6.70%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,32 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num: nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        int ele=-1;
-        int freq=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int num: nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
+        // int ele=-1;
+        // int freq=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.get(nums[i]) > freq || (map.get(nums[i]) == freq) && ele > freq){
+        //         freq=map.get(nums[i]);
+        //         ele=nums[i];
+        //     }
+        // }
+        // return ele;
+
+        int candidate=0;
+        int count=0;
+
         for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i]) > freq || (map.get(nums[i]) == freq) && ele > freq){
-                freq=map.get(nums[i]);
-                ele=nums[i];
+            if(count==0){
+                candidate =nums[i];
+            }
+            if(candidate == nums[i]){
+                count++;
+            }else{
+                count--;
             }
         }
-        return ele;
+        return candidate;
     }
 }

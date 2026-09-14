3271d921f6a5b078c8fd5861ad6dc6fe9aabbe12
Souradip <145707173+souradip23@users.noreply.github.com>
2026-09-14 11:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (95.05%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,7 +3,6 @@ class Solution {
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
-
         while(i>=0 && j>=0){
             if(nums1[i] > nums2[j]){
                 nums1[k]=nums1[i];
@@ -15,11 +14,10 @@ class Solution {
             k--;
         }
 
-        while(j >=0){
+        while(j>=0){
             nums1[k]=nums2[j];
-            k--;
             j--;
+            k--;
         }
-         
     }
 }

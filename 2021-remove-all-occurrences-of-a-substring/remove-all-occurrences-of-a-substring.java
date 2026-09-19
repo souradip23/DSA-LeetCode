535c31bdfaf9957cbda4cd16151039b600ca186c
Souradip <145707173+souradip23@users.noreply.github.com>
2026-09-19 12:17:51 +0530

    Time: 1 ms (99.76%) | Memory: 44.1 MB (68.58%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+       StringBuilder sb=new StringBuilder();
+       sb.append(s);
+        while(sb.toString().contains(part)){
+            int index=sb.indexOf(part);
+            if(index!=-1){
+                sb.delete(index,index+part.length());
+            }
+        } 
+        return sb.toString();
+    }
+}
